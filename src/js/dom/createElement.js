7817972d06ef342b019467669db21651f6cf9812
Mario <marioskrlec222@gmail.com>
2022-12-16 10:04:40 +0100

    Added card animations

--- src/js/dom/createElement.js
+++ src/js/dom/createElement.js
@@ -3,7 +3,20 @@ export function createElement(id, html, className) {
     const parent = rootEl.parentNode;
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = html;
-    rootEl.replaceWith(tempDiv.getElementsByClassName(className)[0]);
+    const appendingElement = tempDiv.getElementsByClassName(className)[0];
+    rootEl.replaceWith(appendingElement);
+
+    const keyframes = [
+        { transform: 'translate3d(0, -5%, 0)', opacity: 0 },
+        { transform: 'translate3d(0, 0, 0)', opacity: 1 },
+    ];
+
+    const timing = {
+        duration: 400,
+        iterations: 1,
+    }
+
+    appendingElement.animate(keyframes, timing)
 
     const newPlaceholder = document.createElement('div');
     newPlaceholder.id = id;
