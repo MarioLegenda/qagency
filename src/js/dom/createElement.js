dcabc96cd72b0f0c067dc6cdbf1cf4dc759c28c7
Mario <marioskrlec222@gmail.com>
2022-12-19 08:22:05 +0100

    Added responsive images

--- src/js/dom/createElement.js
+++ src/js/dom/createElement.js
@@ -1,9 +1,13 @@
-export function createElement(id, html, className) {
+export function createElement(id, html, className, addClassName) {
     const rootEl = document.getElementsByClassName(id)[0];
     const parent = rootEl.parentNode;
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = html;
     const appendingElement = tempDiv.getElementsByClassName(className)[0];
+    if (addClassName) {
+        appendingElement.classList.add(addClassName);
+    }
+
     rootEl.replaceWith(appendingElement);
 
     const keyframes = [
